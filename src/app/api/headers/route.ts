7b0cd81f925c1e0427443a11323896314238f3d4
Reece Walter <reece@reecewalter.com>
2023-11-16 16:45:49 -0500

    fixed api headers

--- src/app/api/headers/route.ts
+++ src/app/api/headers/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(headers: {apiKey: string}) {
+export async function GET(req: NextRequest, headers: {apiKey: string}) {
     let key = headers.apiKey
     let data;
     if (key === 'a1b2c3d4') {
