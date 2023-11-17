2f1b89953035ac517936b72263d6f83edef27995
Reece Walter <reece@reecewalter.com>
2023-11-17 09:03:14 -0500

    added get headers to "api/header" route

--- src/app/api/headers/route.ts
+++ src/app/api/headers/route.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
+import { headers } from 'next/headers'
 
-export async function GET(req: NextRequest, headers: {apiKey: string}) {
-    let key = headers.apiKey
+export async function GET(req: NextRequest) {
+    const headersList = headers();
+    const key = headersList.get('apiKey');
     let data;
     if (key === 'a1b2c3d4') {
-        console.log(key)
         data = 'Welcome to Code'
     } else {
         data = 'Error'
@@ -12,3 +13,6 @@ export async function GET(req: NextRequest, headers: {apiKey: string}) {
     }
     return NextResponse.json({ data })
 }
+
+// curl --header "apiKey: a1b2c3d4" http://localhost:3000/headers
+// curl --header "apiKey: a1b2c3d4" localhost:3000/api/headers
